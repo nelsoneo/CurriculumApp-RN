5284965f311bd441a251ae15b8cd1320bc9d1253
nelsoneo <nelsoneo@gmail.com>
2021-05-24 01:02:17 -0300

    finish cabecalho

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 import foto from './assets/perfil.jpg';
 
 const App = () => {
@@ -11,10 +12,10 @@ const App = () => {
             <Image source={foto} style = {styles.fotoStyle}/>
             <Text style = {styles.nome}>NELSON H GUERRA</Text>
             <Text style = {styles.cargo}>Desenvolvedor Mobile</Text>
-            <View>
-                <Text>GitHub</Text>
-                <Text>Facebook</Text>
-                <Text>Linkdin</Text>
+            <View style = {styles.redesSociais}>
+                <Icon name= 'github' style={{fontSize: 20}} />
+                <Icon name = 'facebook-square' style={{fontSize: 20}} />
+                <Icon name = 'linkedin-square' style={{fontSize: 20}} />
             </View>
         </View>
         {/* <StatusBar style="auto" /> */}
@@ -33,7 +34,8 @@ const styles = StyleSheet.create({
   cabecalhoContainer: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'flex-start',
+    paddingTop: 35
   },
   fotoStyle: {
     width: 250,
@@ -48,6 +50,12 @@ const styles = StyleSheet.create({
   cargo: {
     color: '#933939',
     marginBottom: 10
+  },
+  redesSociais: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '50%',
+    marginTop: 10
   }
 });
 
